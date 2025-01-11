e5306fac0089941f5341736bb57a47acb95e72ea
Floressek <szymon.florek@student.wat.edu.pl>
2025-01-11 01:08:55 +0100

    innit

---
+++ EmailAnalyzer.Client/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EmailAnalyzer.Client;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ EmailAnalyzer.Shared/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace EmailAnalyzer.Shared;
+
+public class Class1
+{
+
+}
